c43c9e049db8184728ccf8e9060e7f6db594183f
Mich103 <michellegathoni3@gmail.com>
2023-02-19 03:27:18 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using printf function
- * return: 0 (passed)
+ * main - prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	int num;
+
+	for (num = 0; num < 10; num++)
+	printf("%d", num);
+
+	printf("\n");
+	return (0);
 }
