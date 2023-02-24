76039c527cfd75db212f482b2c1ab5cb7b809ea9
Mich103 <michellegathoni3@gmail.com>
2023-02-24 12:35:32 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints 0 -9 followed by a new line
+ * return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(0);
+	}
+	_putchar('\n');
+}
