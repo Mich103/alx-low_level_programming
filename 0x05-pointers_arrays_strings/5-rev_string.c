77536aabdbce50b154d874a3cc989d37467bd9d2
Mich103 <michellegathoni3@gmail.com>
2023-02-27 06:58:32 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+*rev_string - function that reverses a string
+*@s: String to reverse
+*Return: 0
+*/
+void rev_string(char *s)
+{
+int i = 0, length;
+length = _strlen(s) - 1;
+while (length > i)
+{
+swap_char(s + length, s + i);
+i++;
+length--;
+}
+}
+
+/**
+ *_strlen - function that returns the length of a string
+ * @s: string
+ *Return: length;
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
+}
+
+/**
+*swap_char - function that swaps two characters
+*@a: first character
+*@b: second character
+*Return: nothing
+*/
+
+void swap_char(char *a, char *b)
+{
+char tmp = *a;
+*a = *b;
+*b = tmp;
+}
