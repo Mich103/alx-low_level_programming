0bc5f68a2ad72d565c2f394d0ae7da2b4f931148
Mich103 <michellegathoni3@gmail.com>
2023-02-27 06:51:23 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,7 @@
 /**
 * print_rev - function that prints a string in reverse
 * @s: string to print
+* Return: 0
 */
 
 void print_rev(char *s)
@@ -13,4 +14,19 @@ for (index = len - 1; index >= 0; index--)
 _putchar(s[index]);
 
 _putchar('\n');
+}
+
+/**
+ *_strlen - function that returns the length of a string
+ * @s: string
+ *Return: length
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
 }
